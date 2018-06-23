8e6c92f9f11bed1d88324a8498e4f477aaef540c
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-23 23:06:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function add(x, y) {
   return x + y 
 }
+
+function subtract(x,y) {
+  return x-a
+}
+
+function multiply(x,y) {
+  return x*y
+}
+
+function divide(x,y) {
+  return x/y 
+}
+
+function inc(n) {
+  return n++
+}
